753a15fbc2930863e59d5547e97ad2a6ab554266
Tamizharuvi <aruvi2907@gmail.com>
2026-02-27 00:05:11 +0530

    Fix case-sensitive imports for Vercel build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 
 import React from 'react'
 import Hero from "../components/HeroSection"
-import Features from "../components/features"
+import Features from "../components/Features"
 import Services from '../components/Services'
-import ChooseUs from '../components/ChooseUs'
+import ChooseUs from '../components/Chooseus'
 import Product from "../components/SaasProduct"
 import Client from "../components/Client"
 import "../index.css"
